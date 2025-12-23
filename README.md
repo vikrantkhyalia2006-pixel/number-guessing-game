ad0145f7edfcebb9b4c63783772bd99cb3523cfe
vikrantkhyalia2006-pixel <vikrantkhyalia2006@gmail.com>
2025-12-23 08:25:24 +0530

    Add number guessing game

---
+++ README.md
@@ -0,0 +1,19 @@
+# Number Guessing Game
+
+## Description
+A simple Python console-based game where the user guesses a randomly generated number between 1 and 100.
+
+## Features
+- Input validation
+- Helpful hints (too high / too low)
+- Counts number of attempts
+
+## Concepts Used
+- Loops
+- Conditionals
+- Functions
+- Exception handling
+- Random module
+
+## How to Run
+python main.py
---
+++ main.py
@@ -0,0 +1,38 @@
+from random import randint
+
+def number_guessing_game():
+    num_guess = randint(1, 100)
+    guess_count = 0
+
+    while True:
+        try:
+            user_input = int(input("Enter a number (1 to 100): "))
+
+        except ValueError:
+            print("Please enter a valid number.")
+            continue # restart loop for invalid input
+
+        if (user_input<1 or user_input>100):
+            print("Number must be between 1 and 100.")
+            continue
+
+        guess_count += 1
+        
+        if(user_input < num_guess):
+            print("too low!")
+
+        elif(user_input > num_guess):
+            print("too high!")  
+
+        else:
+            print(f"Congratulations you guessed the correct number {num_guess} in {guess_count} tries!")
+            break
+
+        play_again = input("Do you want to play again? (y/n): ")
+        if play_again != "y":
+            print("Thanks for playing!")
+            break
+    
+
+    
+number_guessing_game()
